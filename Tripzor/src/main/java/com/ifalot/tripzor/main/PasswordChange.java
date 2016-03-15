0a5a84a3885b13ed58ce8baabbd0b2e9056e9644
piax93 <piax93@hotmail.it>
2016-03-15 14:42:52 +0100

    password change fixed

--- Tripzor/src/main/java/com/ifalot/tripzor/main/PasswordChange.java
+++ Tripzor/src/main/java/com/ifalot/tripzor/main/PasswordChange.java
@@ -1,11 +1,13 @@
 package com.ifalot.tripzor.main;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.ifalot.tripzor.utils.DataManager;
 import com.ifalot.tripzor.utils.FastDialog;
@@ -35,7 +37,8 @@ public class PasswordChange extends AppCompatActivity implements ResultListener{
 						.getText().toString();
 				String passwordConfirm = ((EditText) findViewById(R.id.new_password_confirm))
 						.getText().toString();
-				if(password.length() == 0 || passwordConfirm.length() == 0){
+				String oldPassword = ((EditText) findViewById(R.id.old_password)).getText().toString();
+				if(password.length() == 0 || passwordConfirm.length() == 0 || oldPassword.length() == 0){
 					FastDialog.simpleErrorDialog(PasswordChange.this, "You left some fields empty");
 				}else{
 					if(password.equals(passwordConfirm)){
@@ -43,7 +46,7 @@ public class PasswordChange extends AppCompatActivity implements ResultListener{
 						progressDialog.show();
 						HashMap<String, String> data = new HashMap<String, String>();
 						data.put("action", "ChangePassword");
-						data.put("oldPassword", DataManager.selectData("password"));
+						data.put("oldPassword", oldPassword);
 						data.put("newPassword", password);
 						PostSender.sendPost(data, PasswordChange.this);
 					}else{
@@ -60,10 +63,15 @@ public class PasswordChange extends AppCompatActivity implements ResultListener{
 		if(result.equals(Codes.USER_NOT_FOUND)){
 			FastDialog.simpleErrorDialog(this, "Server Error");
 		}else if(result.equals(Codes.ERROR)){
-			FastDialog.simpleErrorDialog(this, "Bad things");
+			FastDialog.simpleErrorDialog(this, "Old password is probably wrong");
 		}else if(result.equals(Codes.DONE)){
 			DataManager.updateValue("password", password);
-			finish();
+			FastDialog.simpleDialog(this, "Password Changed", "You successfully changed your password", "CLOSE", new MaterialDialog.SingleButtonCallback() {
+				@Override
+				public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
+					PasswordChange.this.finish();
+				}
+			});
 		}
 		
 	}
